7a88271aa895ba34ff82570d6d8b3c1369fa5c42
Alex <aflewwel@ualberta.ca>
2022-11-05 14:57:50 -0600

    the !set command... the !set command is reeeaaallllll

--- events/intro.py
+++ events/intro.py
@@ -1,5 +1,7 @@
-import discord
-from discord.ext import commands
 
-def main(bot):
+async def main(ctx,bot):
+    await ctx.send('test!!!!')
+    await ctx.send('what time would you like to wake up each day?')
+    await ctx.send('use this format: !set mon 06:30')
+    
     
--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import discord
 from discord.ext import commands
-import intro
+from events import intro
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix='!',intents=intents)
+wakeuptimes = {'mon':None,'tue':None,'wed':None,'thu':None,'fri':None,'sat':None,'sun':None}
+
+def checkvalidset(day,time):
+    if day not in wakeuptimes.keys():
+        return False
+    if len(time) != 5:
+        return False
+    for i,letter in enumerate(time):
+        if i == 2:
+            if letter != ':':
+                return False
+        else:
+            if not letter.isnum():
+                return False
+    return True
+
 
 @bot.event
 async def on_ready():
@@ -19,6 +36,19 @@ async def sus(ctx):
 @bot.command()
 async def start(ctx):
     await ctx.send('hi! give me your credit card information')
-    await intro.main(bot)
+    await intro.main(ctx,bot)
+
+@bot.command()
+async def set(ctx,day,time):
+    fullday = {'mon':'monday','tue':'tuesday',
+              'wed':'wednesday','thu':'thursday',
+              'fri':'friday','sat':'saturday',
+              'sun':'sunday'}
+    if checkvalidset(day,time):
+        wakeuptimes[day] = time
+        await ctx.send(f'set wakeup time for {fullday[day]} to {time}')
+    else:
+        await ctx.send('wrong. idiot.')
+
 
 bot.run('MTAzODUxNjcwMjY0NTg1ODM0NA.GfqTZb.3iecHuyKhVpkKalrw6YGODYzGyw9UvC0ASyNi0')
